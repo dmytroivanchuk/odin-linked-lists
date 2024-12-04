1cf3b703dfbc8dec9ecdc9d845d839c12cd22e18
Dmytro Ivanchuk <104496408+dmytroivanchuk@users.noreply.github.com>
2024-12-04 11:59:14 +0200

    feat: add getters to and refactor LinkedList

--- LinkedList.js
+++ LinkedList.js
@@ -1,32 +1,78 @@
 import Node from "./Node.js";
 
 export default class LinkedList {
-  size = 0;
-  head = null;
-  tail = null;
+  #size = 0;
+  #head = null;
+  #tail = null;
+
+  get size() {
+    return this.#size;
+  }
+
+  get head() {
+    return this.#head.value;
+  }
+
+  get tail() {
+    return this.#tail.value;
+  }
+
+  #traverseList(value, callbackTrue, callbackFalse) {
+    let node = this.#head;
+    let index = 0;
+    while (index !== this.#size) {
+      if (node.value === value) {
+        return callbackTrue(index);
+      } else {
+        node = node.nextNode;
+        index += 1;
+      }
+    }
+
+    return callbackFalse();
+  }
+
+  #isInvalidIndex(index, indexValid) {
+    return !Number.isInteger(index) || index < 0 || index > indexValid;
+  }
+
+  #getNodes(index) {
+    let nodeIndex = 0;
+    let node = this.#head;
+    let nodeParent;
+    while (nodeIndex !== index) {
+      if (nodeIndex === index - 1) {
+        nodeParent = node;
+      }
+      node = node.nextNode;
+      nodeIndex += 1;
+    }
+
+    return { node, nodeParent };
+  }
 
   append(value) {
     const newNode = new Node(value);
 
-    if (this.size === 0) {
-      this.head = newNode;
+    if (this.#size === 0) {
+      this.#head = newNode;
     } else {
-      this.tail.nextNode = newNode;
+      this.#tail.nextNode = newNode;
     }
 
-    this.tail = newNode;
-    this.size += 1;
+    this.#tail = newNode;
+    this.#size += 1;
   }
 
   prepend(value) {
-    const newNode = new Node(value, this.head);
+    const newNode = new Node(value, this.#head);
 
-    if (this.size === 0) {
-      this.tail = newNode;
+    if (this.#size === 0) {
+      this.#tail = newNode;
     }
 
-    this.head = newNode;
-    this.size += 1;
+    this.#head = newNode;
+    this.#size += 1;
   }
 
   at(index) {
@@ -34,12 +80,12 @@ export default class LinkedList {
       return undefined;
     }
 
-    if (index < 0 || index > this.size - 1) {
+    if (index < 0 || index > this.#size - 1) {
       return undefined;
     }
 
     let nodeIndex = 0;
-    let node = this.head;
+    let node = this.#head;
     while (nodeIndex !== index) {
       node = node.nextNode;
       nodeIndex += 1;
@@ -49,53 +95,39 @@ export default class LinkedList {
   }
 
   pop() {
-    let node = this.head;
+    let node = this.#head;
     let index = 0;
-    while (index !== this.size - 2) {
+    while (index !== this.#size - 2) {
       node = node.nextNode;
       index += 1;
     }
 
     node.nextNode = null;
-    this.tail = node;
-    this.size -= 1;
+    this.#tail = node;
+    this.#size -= 1;
   }
 
   contains(value) {
-    let node = this.head;
-    let index = 0;
-    while (index !== this.size) {
-      if (node.value === value) {
-        return true;
-      } else {
-        node = node.nextNode;
-        index += 1;
-      }
-    }
-
-    return false;
+    return this.#traverseList(
+      value,
+      () => true,
+      () => false
+    );
   }
 
   find(value) {
-    let node = this.head;
-    let index = 0;
-    while (index !== this.size) {
-      if (node.value === value) {
-        return index;
-      } else {
-        node = node.nextNode;
-        index += 1;
-      }
-    }
-
-    return null;
+    return this.#traverseList(
+      value,
+      (index) => index,
+      () => null
+    );
   }
 
   toString() {
     let string = "";
-    let node = this.head;
+    let node = this.#head;
     let index = 0;
-    while (index !== this.size) {
+    while (index !== this.#size) {
       string += `( ${node.value} ) -> `;
       node = node.nextNode;
       index += 1;
@@ -106,72 +138,40 @@ export default class LinkedList {
   }
 
   insertAt(value, index) {
-    if (!Number.isInteger(index)) {
-      return undefined;
-    }
-
-    if (index < 0 || index > this.size) {
+    if (this.#isInvalidIndex(index, this.#size)) {
       return undefined;
     }
 
     if (index === 0) {
       this.prepend(value);
       return;
-    }
-
-    if (index === this.size) {
+    } else if (index === this.#size) {
       this.append(value);
       return;
     }
 
-    let nodeIndex = 0;
-    let node = this.head;
-    let nodeParent;
-    while (nodeIndex !== index) {
-      if (nodeIndex === index - 1) {
-        nodeParent = node;
-      }
-      node = node.nextNode;
-      nodeIndex += 1;
-    }
-
+    const { node, nodeParent } = this.#getNodes(index);
     const newNode = new Node(value, node);
     nodeParent.nextNode = newNode;
-    this.size += 1;
+    this.#size += 1;
   }
 
   removeAt(index) {
-    if (!Number.isInteger(index)) {
-      return undefined;
-    }
-
-    if (index < 0 || index > this.size - 1) {
+    if (this.#isInvalidIndex(index, this.#size - 1)) {
       return undefined;
     }
 
     if (index === 0) {
-      this.head = this.head.nextNode;
-      this.size -= 1;
+      this.#head = this.#head.nextNode;
+      this.#size -= 1;
       return;
-    }
-
-    if (index === this.size - 1) {
+    } else if (index === this.#size - 1) {
       this.pop();
       return;
     }
 
-    let nodeIndex = 0;
-    let node = this.head;
-    let nodeParent;
-    while (nodeIndex !== index) {
-      if (nodeIndex === index - 1) {
-        nodeParent = node;
-      }
-      node = node.nextNode;
-      nodeIndex += 1;
-    }
-
+    const { node, nodeParent } = this.#getNodes(index);
     nodeParent.nextNode = node.nextNode;
-    this.size -= 1;
+    this.#size -= 1;
   }
 }
